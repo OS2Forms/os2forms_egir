3ed06bffaf8aa1b0b9b6f19d96031360008ce267
Carsten Agger <carstena@magenta-aps.dk>
2021-10-27 09:33:31 +0200

    Added test for getTermData and formsLog etc.

--- tests/unit/GIRUtilsTest.php
+++ tests/unit/GIRUtilsTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Drupal\user\Entity\User;
+use Drupal\taxonomy\Entity\Term;
 
 use Drupal\os2forms_egir\GIRUtils;
 
@@ -21,25 +22,71 @@ class GIRUtilsTest extends \Codeception\Test\Unit
     }
 
     // tests
+    public function testFormsLog() {
+      $logger = GIRUtils::formsLog();
+      $this->assertTrue(method_exists($logger, 'notice'));
+      $this->assertTrue(method_exists($logger, 'error'));
+
+    }
     public function testGetUserData()
     {
         $language = \Drupal::languageManager()->getCurrentLanguage()->getId();
           
         $user = \Drupal\user\Entity\User::create();
         $user->enforceIsNew();
-          $user->setPassword('password');
-          $user->setEmail('egir@example.com');
-          $user->setUsername('user');
-          $user->set("init", 'mail');
-          $user->set("langcode", $language);
-          $user->set("preferred_langcode", $language);
-          $user->set("preferred_admin_langcode", $language);
-          $user->activate();
-          $user->save();
+        $user->setPassword('password');
+        $user->setEmail('egir@example.com');
+        $user->setUsername('user');
+        $user->activate();
+        $user->save();
       $name = GIRUtils::getUserData($user->id(), 'name');
           $this->assertEquals($name, 'user');
 
           $user->delete();
 
     }
+
+    public function testGetTermData()
+    {
+      $term = Term::create(['name' => 'egir_test', 'vid' => 'client']);
+      $term->save();
+
+      $name = GIRUtils::getTermData($term->id(), 'name');
+
+      $this->assertEquals($name, 'egir_test');
+
+      $term->delete();
+
+    }
+
+    public function testGetTermIdByName()
+    {
+      $term = Term::create(['name' => 'test_id', 'vid' => 'client']);
+      $term->save();
+      $id = $term->id();
+
+      $storedId = GIRUtils::getTermIdByName('test_id');
+
+      $this->assertEquals($id, $storedId);
+
+    }
+
+    public function testGetUserByGirUuid()
+    {
+      $uuid = 'a6207d6a-36ff-11ec-8c87-6763965e91e0';
+      // Create new user.
+      $user = \Drupal\user\Entity\User::create();
+      $user->enforceIsNew();
+      $user->setPassword('password');
+      $user->setEmail('uuid_test@example.com');
+      $user->setUsername('uuid_test');
+      $user->activate();
+      $user->field_uuid = $uuid;
+      $user->save();
+
+      $stored_user_id = GIRUtils::getUserByGirUuid($uuid);
+
+      $this->assertEquals($stored_user_id, $user->id());
+    }
+
 }
